81a554732857050a30ebbdbf7a5f9b0bbdcd5499
Vladimir_135 <bvladimirv135@gmail.com>
2020-07-10 09:03:59 +0300

    Почему-то не запушились файлы...

---
+++ User/ble_doseIO_s.c
@@ -0,0 +1,148 @@
+#include "sdk_common.h"
+#include "ble_doseIO_s.h"
+#include "ble_srv_common.h"
+
+
+
+/**@brief Function for handling the Write event.
+ *
+ * @param[in] p_doseIO      LED Button Service structure.
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ */
+static void on_write(ble_doseIO_t * p_doseIO, ble_evt_t const * p_ble_evt)
+{
+    ble_gatts_evt_write_t const * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+
+    if (   (p_evt_write->handle == p_doseIO->led_char_handles.value_handle)
+        && (p_evt_write->len == 1)
+        && (p_doseIO->led_write_handler != NULL))
+    {
+        p_doseIO->led_write_handler(p_ble_evt->evt.gap_evt.conn_handle, p_doseIO, p_evt_write->data[0]);
+    }
+}
+
+
+void ble_doseIO_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context)
+{
+    ble_doseIO_t * p_doseIO = (ble_doseIO_t *)p_context;
+
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GATTS_EVT_WRITE:
+            on_write(p_doseIO, p_ble_evt);
+            break;
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+
+uint32_t ble_doseIO_init(ble_doseIO_t * p_doseIO, const ble_doseIO_init_t * p_doseIO_init)
+{
+    volatile uint32_t              err_code;
+    ble_uuid_t            ble_uuid;
+    ble_add_char_params_t add_char_params;
+
+    // Initialize service structure.
+    p_doseIO->led_write_handler = p_doseIO_init->led_write_handler;
+
+    // Add service.
+    ble_uuid128_t base_uuid = {doseIO_UUID_BASE};
+    err_code = sd_ble_uuid_vs_add(&base_uuid, &p_doseIO->uuid_type);
+    VERIFY_SUCCESS(err_code);
+
+    ble_uuid.type = p_doseIO->uuid_type;
+    ble_uuid.uuid = doseIO_UUID_SERVICE;
+
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &p_doseIO->service_handle);
+    VERIFY_SUCCESS(err_code);
+
+    // Add Button characteristic.
+    memset(&add_char_params, 0, sizeof(add_char_params));
+    add_char_params.uuid              = doseIO_UUID_BUTTON_CHAR;
+    add_char_params.uuid_type         = p_doseIO->uuid_type;
+    add_char_params.init_len          = sizeof(uint16_t);
+    add_char_params.max_len           = sizeof(uint16_t);
+    add_char_params.char_props.read   = 1;
+    add_char_params.char_props.notify = 1;
+
+    add_char_params.read_access       = SEC_OPEN;
+    add_char_params.cccd_write_access = SEC_OPEN;
+
+    err_code = characteristic_add(p_doseIO->service_handle,
+                                  &add_char_params,
+                                  &p_doseIO->button_char_handles);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+		
+		memset(&add_char_params, 0, sizeof(add_char_params));
+    add_char_params.uuid              = doseIO_UUID_BUTTON_CHAR2;
+    add_char_params.uuid_type         = p_doseIO->uuid_type;
+    add_char_params.init_len          = sizeof(uint16_t);
+    add_char_params.max_len           = sizeof(uint16_t);
+    add_char_params.char_props.read   = 1;
+    add_char_params.char_props.notify = 1;
+
+    add_char_params.read_access       = SEC_OPEN;
+    add_char_params.cccd_write_access = SEC_OPEN;
+
+    err_code = characteristic_add(p_doseIO->service_handle,
+                                  &add_char_params,
+                                  &p_doseIO->button_char_handles2);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+		
+		memset(&add_char_params, 0, sizeof(add_char_params));
+    add_char_params.uuid              = doseIO_UUID_BUTTON_CHAR2+5;
+    add_char_params.uuid_type         = p_doseIO->uuid_type;
+    add_char_params.init_len          = sizeof(uint16_t);
+    add_char_params.max_len           = sizeof(uint16_t);
+    add_char_params.char_props.read   = 1;
+    add_char_params.char_props.notify = 1;
+
+    add_char_params.read_access       = SEC_OPEN;
+    add_char_params.cccd_write_access = SEC_OPEN;
+
+    err_code = characteristic_add(p_doseIO->service_handle,
+                                  &add_char_params,
+                                  &p_doseIO->button_char_handles2);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+
+    // Add LED characteristic.
+    memset(&add_char_params, 0, sizeof(add_char_params));
+    add_char_params.uuid             = doseIO_UUID_LED_CHAR;
+    add_char_params.uuid_type        = p_doseIO->uuid_type;
+    add_char_params.init_len         = sizeof(uint8_t);
+    add_char_params.max_len          = sizeof(uint8_t);
+    add_char_params.char_props.read  = 1;
+    add_char_params.char_props.write = 1;
+
+    add_char_params.read_access  = SEC_OPEN;
+    add_char_params.write_access = SEC_OPEN;
+
+    return characteristic_add(p_doseIO->service_handle, &add_char_params, &p_doseIO->led_char_handles);
+}
+
+
+uint32_t ble_doseIO_on_button_change(uint16_t conn_handle, ble_doseIO_t * p_doseIO, uint8_t button_state)
+{
+    ble_gatts_hvx_params_t params;
+    uint16_t len = sizeof(button_state);
+
+    memset(&params, 0, sizeof(params));
+    params.type   = BLE_GATT_HVX_NOTIFICATION;
+    params.handle = p_doseIO->button_char_handles.value_handle;
+    params.p_data = &button_state;
+    params.p_len  = &len;
+
+    return sd_ble_gatts_hvx(conn_handle, &params);
+}
---
+++ User/ble_doseIO_s.h
@@ -0,0 +1,96 @@
+#ifndef BLE_DOSEIO_S_H__
+#define BLE_DOSEIO_S_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+#include "nrf_sdh_ble.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+	/**@brief   Macro for defining a ble_doseIO instance.
+ *
+ * @param   _name   Name of the instance.
+ * @hideinitializer
+ */
+#define BLE_doseIO_DEF(_name)                                                                          \
+static ble_doseIO_t _name;                                                                             \
+NRF_SDH_BLE_OBSERVER(_name ## _obs,                                                                 	 \
+                     BLE_doseIO_BLE_OBSERVER_PRIO,                                                     \
+                     ble_doseIO_on_ble_evt, &_name)
+	
+//15f2e42e-c26d-11ea-b3de-0242ac130004
+#define doseIO_UUID_BASE					{0x04, 0x00, 0x13, 0xac, 0x42, 0x02, 0xde, 0xb3, \
+																	 0xea, 0x11, 0x6d, 0xc2, 0x2e, 0xe4, 0xf2, 0x15}
+						
+#define doseIO_UUID_SERVICE     	0xc9d0
+#define doseIO_UUID_BUTTON_CHAR 	0xc9d1
+#define doseIO_UUID_BUTTON_CHAR2 	0xc9d2
+#define doseIO_UUID_LED_CHAR    	0xc9d3
+
+
+// Forward declaration of the ble_doseIO_t type.
+typedef struct ble_doseIO_s ble_doseIO_t;
+
+typedef void (*ble_doseIO_led_write_handler_t) (uint16_t conn_handle, ble_doseIO_t * p_doseIO, uint8_t new_state);
+
+/** @brief LED Button Service init structure. This structure contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
+{
+    ble_doseIO_led_write_handler_t led_write_handler; /**< Event handler to be called when the LED Characteristic is written. */
+} ble_doseIO_init_t;
+
+/**@brief LED Button Service structure. This structure contains various status information for the service. */
+struct ble_doseIO_s
+{
+    uint16_t                    service_handle;      /**< Handle of LED Button Service (as provided by the BLE stack). */
+    ble_gatts_char_handles_t    led_char_handles;    /**< Handles related to the LED Characteristic. */
+    ble_gatts_char_handles_t    button_char_handles; /**< Handles related to the Button Characteristic. */
+		ble_gatts_char_handles_t    button_char_handles2; /**< Handles related to the Button Characteristic. */
+    uint8_t                     uuid_type;           /**< UUID type for the LED Button Service. */
+    ble_doseIO_led_write_handler_t led_write_handler;   /**< Event handler to be called when the LED Characteristic is written. */
+};
+
+
+/**@brief Function for initializing the LED Button Service.
+ *
+ * @param[out] p_doseIO      LED Button Service structure. This structure must be supplied by
+ *                        the application. It is initialized by this function and will later
+ *                        be used to identify this particular service instance.
+ * @param[in] p_doseIO_init  Information needed to initialize the service.
+ *
+ * @retval NRF_SUCCESS If the service was initialized successfully. Otherwise, an error code is returned.
+ */
+uint32_t ble_doseIO_init(ble_doseIO_t * p_doseIO, const ble_doseIO_init_t * p_doseIO_init);
+
+
+/**@brief Function for handling the application's BLE stack events.
+ *
+ * @details This function handles all events from the BLE stack that are of interest to the LED Button Service.
+ *
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ * @param[in] p_context  LED Button Service structure.
+ */
+void ble_doseIO_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context);
+
+
+/**@brief Function for sending a button state notification.
+ *
+ ' @param[in] conn_handle   Handle of the peripheral connection to which the button state notification will be sent.
+ * @param[in] p_doseIO         LED Button Service structure.
+ * @param[in] button_state  New button state.
+ *
+ * @retval NRF_SUCCESS If the notification was sent successfully. Otherwise, an error code is returned.
+ */
+uint32_t ble_doseIO_on_button_change(uint16_t conn_handle, ble_doseIO_t * p_doseIO, uint8_t button_state);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BLE_DOSEIO_S_H__
